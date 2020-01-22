61e5b56a358b62e851e9d90db72a1014b2ea17d2
qiuxiang <liumeiwang@localhost.com>
2020-01-22 11:30:26 +0800

    1

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    pages:{
+        A:{
+            entry:'src/views/A/main.js',
+            template: 'public/A.html',
+            filename: 'A.html'
+        },
+        B:{
+            entry:'src/views/B/main.js',
+            template : 'public/index.html',
+            filename: 'index.html'
+        }
+    }
+}
